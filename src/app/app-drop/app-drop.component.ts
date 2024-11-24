ba0403fe859a7d6eba1aa6ebfafe8db8fc470a2f
Орехов Андрей <aorehov@softrust.ru>
2024-11-24 22:55:47 +0300

    Наработки по компоненту дропа

---
+++ src/app/app-drop/app-drop.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-drop',
+  imports: [NgOptimizedImage],
+  templateUrl: './app-drop.component.html',
+  styleUrl: './app-drop.component.scss',
+})
+export class AppDropComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppDropComponent} from './app-drop/app-drop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'test', component: AppDropComponent}];
